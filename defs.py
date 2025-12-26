8b6df93c6f99ef8fdd06e57ef6be1eb66e183503
Magzhan <159104599+zzmaga@users.noreply.github.com>
2025-12-27 02:02:07 +0500

    Update defs.py

--- defs.py
+++ defs.py
@@ -14,7 +14,7 @@ def number_to_string(num): return str(num)
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡘⣿⣿⣿⣿⣿⣿⣿⣿⡇⣼⣿⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⢸⣿⣿⣿⣿⣿⣿⣿⠁⣿⣿⣿⣿⣿⣿⣿⣿⣿
 '''
-
+--------------------------------------------------------------------------------------------
 // 1 var
 def positive_sum(arr):
     i = 0
@@ -37,3 +37,21 @@ def positive_sum(arr):
 // 3 var
 def positive_sum(arr):
     return sum(x for x in arr if x > 0)
+
+--------------------------------------------------------------------------------------------
+// Simple multiplication
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise
+    
+// var 1
+def simple_multiplication(number) :
+    return number * 9 if number % 2 else number * 8 // number % 2 == 1 (True) number %2 == 0 (False)
+
+// var 2
+def simple_multiplication(n):
+    return n * (8 + n % 2) // n % 2 == 0 (8+0), n % 2 == 1 (8+1)
+
+// var 3
+def simple_multiplication(number):
+    return number * 8 if number % 2 == 0 else number * 9
+
+--------------------------------------------------------------------------------------------
