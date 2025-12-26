a1fcae17b693457baf54c8f210f1b2a42349b052
Magzhan <159104599+zzmaga@users.noreply.github.com>
2025-12-27 01:23:04 +0500

    Refactor positive_sum to use generator expression
    Refactor positive_sum function to improve efficiency.

--- defs.py
+++ defs.py
@@ -14,3 +14,26 @@ def number_to_string(num): return str(num)
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡘⣿⣿⣿⣿⣿⣿⣿⣿⡇⣼⣿⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⢸⣿⣿⣿⣿⣿⣿⣿⠁⣿⣿⣿⣿⣿⣿⣿⣿⣿
 '''
+
+// 1 var
+def positive_sum(arr):
+    i = 0
+    total = 0
+    while i < len(arr):
+        if arr[i] > 0:
+            total += arr[i]
+        i += 1
+
+    return total
+
+// 2 var
+def positive_sum(arr):
+    total = 0
+    for x in arr:
+        if x > 0:
+            total += x
+    return total
+
+// 3 var
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
