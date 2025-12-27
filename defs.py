47ee76045a62177cad721d07a8f7fdc791468caf
Magzhan <159104599+zzmaga@users.noreply.github.com>
2025-12-27 14:31:13 +0500

    Implement area_or_perimeter function
    Add function to calculate area or perimeter based on dimensions

--- defs.py
+++ defs.py
@@ -55,3 +55,16 @@ def simple_multiplication(number):
     return number * 8 if number % 2 == 0 else number * 9
 
 --------------------------------------------------------------------------------------------
+""" You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+Example(Input1, Input2 --> Output):
+
+6, 10 --> 32
+3, 3 --> 9
+Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle. """
+
+def area_or_perimeter(l, w):
+    return l * w if l == w else (l + w) * 2
+
+--------------------------------------------------------------------------------------------
